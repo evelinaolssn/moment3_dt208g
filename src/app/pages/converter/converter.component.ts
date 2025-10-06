8044675b6a327b1bc8fd18182576ac99ddb45d55
Evelina Olsson <evol2401@student.miun.se>
2025-10-06 16:03:48 +0200

    Add converter functionality with ngModel and pipes

--- src/app/pages/converter/converter.component.ts
+++ src/app/pages/converter/converter.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './converter.html',
   styleUrl: './converter.css'
 })
 export class ConverterComponent {
-
+  meters: number = 0;
+  celsius: number = 0;
 }
